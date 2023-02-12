33e4966f25e72c55f95dad757395a9d7e990ef20
Daisy Simiyu <d.daisysimiyu@gmail.com>
2023-02-12 05:36:11 +0300

    seeding database

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :accounts
+  has_many :cards
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# db/seeds.rb
+
+# Create customers
+customers = [
+    { first_name: "John", last_name: "Doe", email: "johndoe@example.com" },
+    { first_name: "Jane", last_name: "Doe", email: "janedoe@example.com" },
+    { first_name: "Bob", last_name: "Smith", email: "bobsmith@example.com" }
+  ]
+  
+  customers.each do |customer_attributes|
+    customer = Customer.create!(customer_attributes)
+  
+    # Create accounts for each customer
+    2.times do
+      customer.accounts.create!(balance: 100.0)
+    end
+  
+    # Create cards for each customer
+    3.times do
+      customer.cards.create!(card_number: rand(100000000000000..9999999999999999))
+    end
+  end
+  
+  puts "Seeded #{Customer.count} customers, #{Account.count} accounts, and #{Card.count} cards"
+  
+
+
