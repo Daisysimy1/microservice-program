77a0da63c4c9f7bcd43726c254373c9c4465b606
Daisy Simiyu <d.daisysimiyu@gmail.com>
2023-02-12 05:02:39 +0300

    remove files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  resources :cards
-  resources :accounts
   resources :customers
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
