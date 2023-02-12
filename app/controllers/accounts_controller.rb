a5ed75afd56cad42aabb7b2f706a8f98bc76ddfb
Daisy Simiyu <d.daisysimiyu@gmail.com>
2023-02-12 05:55:34 +0300

    account route

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,19 @@
 class AccountsController < ApplicationController
+    before_action :set_account, only: [:top_up, :withdraw]
+  
+    def top_up
+      @account.top_up
+      render json: @account, status: :ok
+    end
+  
+    def withdraw
+      @account.withdraw
+      render json: @account, status: :ok
+    end
+  
+    private
+  
+    def set_account
+      @account = Account.find(params[:id])
+    end
 end
